72554f36582817d788577f36a52caae8226dbe0a
jannik brack <jannik.brack@wheregroup.com>
2025-06-27 12:26:37 +0200

    fix export in main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,3 @@
 export {AlertComponent} from './components/AlertComponent';
 export type { AlertMessage } from './components/AlertComponent';
-export { MessengerHelper } from './helper/MessengerHelper.ts';
+export { MessengerHelper } from './helper/MessengerHelper';
